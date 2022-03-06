59451a06e6aa419f0e2cf06370b217c4e62bc97d
MSBivens <mikebiv95@gmail.com>
2022-03-06 13:10:21 -0600

    updated files

--- nested-arrays/script.js
+++ nested-arrays/script.js
@@ -1,25 +1,29 @@
-// Write a function that returns true if some value is present and false otherwise
 
 // We can console.log the `8` in this array if we
 // console.log(arr2d[0][2]);
 // the first index `0` will select the `[2, 5, 8]` sub-array
 // the second index `2` will select the `8` out of that sub-array
 
+// Write a function that returns true if some value is present and false otherwise
+
 var arr2d = [ [2, 5, 8],
 [3, 6, 1],
 [5, 7, 7] ];
 
-var testnum = 2
+var testnum = 7
 
 function testif(testnum) {
     for (var i=0; i<arr2d.length; i++) {
-        for (var i; i<arr2d.length[i]; i++) {
-            if (arr2d(i) == testnum) {
-                console.log(Boolean)
+        for (var b=0; b<arr2d[i].length; b++) {
+            if (arr2d[i][b] == testnum) {
+                return(true);
             } 
         }
     }
+    return(false);
 }
+
+console.log(testif(testnum));
 
 
 // function testif(test) {
