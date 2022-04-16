40bb8a8859b1939348ae1e3066e9f25cab6eea6d
MSBivens <mikebiv95@gmail.com>
2022-04-15 21:05:01 -0500

    Update with new JS snippit

--- java_script/js_snippits/script.js
+++ java_script/js_snippits/script.js
@@ -7,3 +7,18 @@ var summation = function (num) {
     }
     return result;
 }
+
+// Fake Binary
+// Given string of digits, replace any digit  below 5 with '0' and any digit 5 and above with '1'. Return the resulting string
+function fakeBin(str){
+    var newStr = "";
+    for(var i=0;i<str.length;i++){
+        if(Number(str[i])>=5){
+        newStr += "1"
+        }
+        else{
+        newStr += "0";
+        }
+    }
+    return newStr;
+}
