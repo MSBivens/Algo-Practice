b3d8bf791a6d97f3701c0c9f7692e440629f0350
MSBivens <mikebiv95@gmail.com>
2022-04-16 22:19:38 -0500

    JS snippit practice

--- java_script/js_snippits/script.js
+++ java_script/js_snippits/script.js
@@ -22,3 +22,12 @@ function fakeBin(str){
     }
     return newStr;
 }
+
+// Reversed Sequence
+//Build a function that returns an array of integers from n to 1 where n>0
+const reverseSeq = n => {
+    let arr = [];
+        for (let i=n; i>0; i--) {
+        arr.push(i);
+        } return arr;
+    };
