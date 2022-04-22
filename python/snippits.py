4812c862f959a321aacc71d9e23b453091819e0f
MSBivens <mikebiv95@gmail.com>
2022-04-21 19:39:11 -0500

    Daily Python practice

--- python/snippits.py
+++ python/snippits.py
@@ -1,2 +1,4 @@
 # Convert number to reversed array of digits
 # Given a random non-negative number, return the digits of the number within an array in reverse order
+def digitize(n):
+    return [int(x) for x in reversed(str(n))]
