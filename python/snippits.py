3cb32f05332e93641c0f5340245a17cdb13c229f
MSBivens <mikebiv95@gmail.com>
2022-04-23 13:40:14 -0500

    Update with daily practice

--- python/snippits.py
+++ python/snippits.py
@@ -6,3 +6,8 @@ def digitize(n):
 # Check if array [a] contains value [x], return boolean 
 def check(seq, elem):
     return elem in seq
+
+# Given an array of integers, return a new array with each value doubled.
+# Ex [1, 2, 3] -> [2, 4, 6]
+def maps(a):
+    return [x * 2 for x in a]
