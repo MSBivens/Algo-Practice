973d2982a3cdb493aef4b3a3584c22db04984b13
MSBivens <mikebiv95@gmail.com>
2022-04-22 20:05:29 -0500

    Update with practice

--- python/snippits.py
+++ python/snippits.py
@@ -2,3 +2,7 @@
 # Given a random non-negative number, return the digits of the number within an array in reverse order
 def digitize(n):
     return [int(x) for x in reversed(str(n))]
+
+# Check if array [a] contains value [x], return boolean 
+def check(seq, elem):
+    return elem in seq
