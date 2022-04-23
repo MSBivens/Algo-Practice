1617c44377885f149acd01a6e2101fadd5f9aa56
MSBivens <mikebiv95@gmail.com>
2022-04-23 13:52:12 -0500

    Update with daily practice

--- solidity/snippits.sol
+++ solidity/snippits.sol
@@ -10,3 +10,14 @@ contract CountByX {
         return z;
     }
 }
+
+// Clock
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Write a function which returns the time since midnight in milliseconds.
+pragma solidity ^0.4.19;
+
+contract Kata {
+    function past(uint h, uint m, uint s) public pure returns (uint) {
+        return 1000*(h*3600+m*60+s);
+    }
+}
