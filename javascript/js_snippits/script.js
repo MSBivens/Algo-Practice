4ebe0468a1fb8286c563632152dd00fb350de9b0
MSBivens <mikebiv95@gmail.com>
2022-04-23 13:27:29 -0500

    Update with daily JS practice

--- javascript/js_snippits/script.js
+++ javascript/js_snippits/script.js
@@ -124,3 +124,10 @@ console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 function numberToString(num) {
     return num.toString();
 }
+
+// Are you playing Banjo?
+// Ccreate a function which answers the question "Are you playing Banjo?"
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
