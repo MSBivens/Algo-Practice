8bc01d934e1a0e08a842466b46d74865cac4071d
MSBivens <mikebiv95@gmail.com>
2022-04-25 17:55:04 -0500

    Solidity practice

--- solidity/snippits.sol
+++ solidity/snippits.sol
@@ -49,3 +49,12 @@ contract ThirdAngle {
         return 180-(angle1+angle2);
     }
 }
+
+// Function tests if the factor is factor of base, return Boolean T/F
+pragma solidity ^0.4.19;
+
+contract Kata {
+    function checkForFactor(int base, int factor) public pure returns (bool) {
+        return base % factor == 0;
+    }
+}
