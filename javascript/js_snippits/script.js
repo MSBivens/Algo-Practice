953c4ea93ba210680a036ce118ff1d6d03c54a38
MSBivens <mikebiv95@gmail.com>
2022-04-26 20:55:15 -0500

    practice

--- javascript/js_snippits/script.js
+++ javascript/js_snippits/script.js
@@ -151,3 +151,6 @@ function booleanToString(b){
 function DNAtoRNA(dna) {
     return dna.replace(/T/g, 'U');
 }
+
+// Function that takes speed in km/hr and returns it in cm/s rounded down to the integer
+const speed = s => Math.floor(s/0.036);
