1f021a6224f54e188c960bb55c50c5ff47af9175
MSBivens <mikebiv95@gmail.com>
2022-04-26 20:57:59 -0500

    practice

--- python/snippits.py
+++ python/snippits.py
@@ -24,3 +24,12 @@ def invert(lst):
 # Convert string to a number
 def string_to_number(s):
     return int(s)
+
+# Return winner of rock, paper, scissors, or draw
+def rps(p1, p2):
+    beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    if beats[p1] == p2:
+        return "Player 1 won!"
+    if beats[p2] == p1:
+        return "Player 2 won!"
+    return "Draw!"
