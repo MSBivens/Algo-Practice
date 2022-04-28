2e707ca4088b949d867a8857b435f37d3f6d9d76
MSBivens <mikebiv95@gmail.com>
2022-04-27 20:12:36 -0500

    practice

--- solidity/snippits.sol
+++ solidity/snippits.sol
@@ -60,3 +60,12 @@ contract Kata {
         return p + (r*2);
     }
 }
+
+// Given a year, return the century it is in.
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+pragma solidity ^0.4.19;
+contract Kata {
+    function century(int year) public pure returns (int) {
+        return (year + 99) / 100 | 0;
+    }
+}
