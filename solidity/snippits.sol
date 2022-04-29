4017ad5bbec66c3705dfbb2c09505df831a755c4
MSBivens <mikebiv95@gmail.com>
2022-04-29 18:39:26 -0500

    practice

--- solidity/snippits.sol
+++ solidity/snippits.sol
@@ -69,4 +69,11 @@ contract Kata {
     }
 }
 
-// Count by x
+// Sum of odd numbers
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+pragma solidity ^0.4.19;
+contract Kata {
+    function rowSumOddNumbers(uint n) public returns (uint) {
+        return n ** 3;
+    }
+}
