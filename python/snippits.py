c7714bc7a0577f6cb69633fe2a35b612bc187b2a
MSBivens <mikebiv95@gmail.com>
2022-04-29 18:39:19 -0500

    practice

--- python/snippits.py
+++ python/snippits.py
@@ -44,3 +44,14 @@ def bool_to_word(boolean):
 # If the array does not contain any numbers then you should return 0.
 def sum_array(a):
     return sum(a)
+
+# Transportation on vacation
+# Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+# Write a code that gives out the total amount for different days(d).
+def rental_car_cost(days):
+    price = days*40
+    if days >= 7:
+        price = (days*40) - 50
+    elif days >= 3:
+        price = (days*40) - 20
+    return price
