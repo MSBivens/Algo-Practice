4215daf4ba12b4757af8855d0e9a1be8172cb521
MSBivens <mikebiv95@gmail.com>
2022-04-28 20:54:53 -0500

    practice

--- solidity/snippits.sol
+++ solidity/snippits.sol
@@ -2,7 +2,6 @@
 // Create a function with two arguments that will return an array of the first (n) multiples of (x). Return as an array
 pragma solidity ^0.4.19;
 contract CountByX {
-
     function countBy(int x, int n) view returns (int[]) {
         int[] z;
         for (int i=1; i <= n; i ++) {
@@ -69,3 +68,5 @@ contract Kata {
         return (year + 99) / 100 | 0;
     }
 }
+
+// Count by x
