ff80502c1a1bcd9583e5f7219fa86afbe7d40158
MSBivens <mikebiv95@gmail.com>
2022-04-29 18:23:51 -0500

    practice

--- javascript/js_snippits/script.js
+++ javascript/js_snippits/script.js
@@ -164,3 +164,21 @@ function squareSum(numbers){
 function betterThanAverage(classPoints, yourPoints) {
     return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
 }  
+
+// Sum without highest and lowest number (by value, not index)
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+function sumArray(array) {
+    if (array == null) {
+        return 0;
+    } else if (array.length < 2) {
+        return 0;
+    } else {
+        array = array.sort(function(a,b) {return a - b;});
+        var total = 0;
+        for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+    }
+        return total;
+    }
+}
+
